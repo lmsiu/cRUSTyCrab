9a072179fef9ac3b2de280920809a6513da31650
lmsiu <lmsiu@cpp.edu>
2024-05-05 18:19:55 -0700

    Add collision for the crab and walls (yipee!)

--- src/bin/gameone.rs
+++ src/bin/gameone.rs
@@ -1,4 +1,5 @@
-use bevy::{prelude::*, math::vec3};
+use std::char::from_u32;
+use bevy::{prelude::*, math::vec3, };
 
 // Constant Variables
 
@@ -30,7 +31,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup) //these systems are really just functions
-        .add_systems(FixedUpdate, (move_paddle, apply_velocity))// runs at a fixed rate
+        .add_systems(FixedUpdate, (move_paddle, apply_velocity, check_ball_collisions.after(apply_velocity),))// runs at a fixed rate
         .run()
 }
 
@@ -39,7 +40,9 @@ fn main() {
 struct Paddle;
 
 #[derive(Component)]
-struct Crab;
+struct Crab{
+    size: Vec2,
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
@@ -92,7 +95,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
             texture: crab_texture,
             ..default()
         },
-         Crab,
+         Crab{ size: CRAB_SIZE },
         Velocity(CRAB_SPEED * CRAB_INITIAL_DIRECTION)
         ) // Add paddle component to the player
     );
@@ -198,7 +201,11 @@ fn move_paddle(
         direction += 1.0;
     }
 
-    let new_x = paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.delta_seconds();
+    let mut new_x: f32 = paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.delta_seconds();
+
+    // "Collision"
+    new_x = new_x.min(RIGHT_WALL - (WALL_THICKNESS+PADDLE_SIZE.x) * 0.5); // take either the new x position or the wall
+    new_x = new_x.max(LEFT_WALL + (WALL_THICKNESS+PADDLE_SIZE.x) * 0.5); // take either the new x position or the wall position
 
     paddle_transform.translation.x = new_x;
 }
@@ -211,6 +218,65 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<
     }
 
 }
+
+fn check_ball_collisions(
+    mut crab_query: Query<(&mut Velocity, &Transform, &Crab)>,
+    collider_query: Query<(&Transform, &Collider)>,
+){
+    let crab_half_size: f32 = CRAB_SIZE.x/2.0; // crab is same size both directions
+    let x_min: f32 = LEFT_WALL + (WALL_THICKNESS+CRAB_SIZE.x) * 0.5;
+    let x_max: f32 = RIGHT_WALL - (WALL_THICKNESS+CRAB_SIZE.x) * 0.5;
+    let y_min: f32 = BOTTOM_WALL + (WALL_THICKNESS+CRAB_SIZE.y) * 0.5;
+    let y_max: f32 = TOP_WALL - (WALL_THICKNESS+CRAB_SIZE.y) * 0.5;
+
+    for(mut crab_velocity, crab_transform, crab) in &mut crab_query {
+        let translation: Vec3 = crab_transform.translation;
+
+        if (translation.x < x_min || translation.x > x_max) {
+            crab_velocity.x *= -1.
+        }
+
+        if (translation.y < y_min || translation.y > y_max) {
+            crab_velocity.y *= -1.;
+        }
+    }
+
+
+
+/*
+        for(transform, other) in &collider_query{
+
+
+
+            // check if there is a collision and reflect in the proper direction if so
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            //check if there is a collision
+            if let Some(collision) = collision{
+                match collision {
+                    Collision::Left => reflect_x = crab_velocity.x > 0.0,
+                    Collision::Right => reflect_x = crab_velocity.x < 0.0,
+                    Collision::Top => reflect_y = crab_velocity.y < 0.0,
+                    Collision::Bottom => reflect_y = crab_velocity.y > 0.0,
+                    Collision::Inside => { /*do nothing*/ }
+                }
+
+                // reflect where needed if there is a collision
+                if reflect_x{
+                    crab_velocity.x *= -1.; // reflect in the opposite direction
+                }
+                if reflect_y{
+                    crab_velocity.y *= -1.;
+                }
+
+            }
+
+        }
+        */
+
+}
+
 
 
 
