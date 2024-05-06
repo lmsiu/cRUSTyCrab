c7c8d2dba7a85113e742fd8ef3f9c2fb0434df84
johnathanmitri <johnathanmitri@gmail.com>
2024-05-06 02:26:33 -0700

    sort of a game now
    Items are dropped in random positions now. Screen size is respected by both the player and the items that drop. Also (usually) responds to resizing, and has a background.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::{prelude::*, time::common_conditions::on_timer};
 use bevy::render::render_resource::Texture;
-use bevy::window::WindowResized;
+use bevy::window::{PresentMode, WindowResized};
 use rand::Rng;
 
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
@@ -9,14 +9,13 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 static mut WIDTH: f32 = 1280.0;
 static mut HEIGHT: f32 = 720.0;
 
-//const PROJECTILE_WIDTH: i32 = 100;
-//const PROJECTILE_HEIGHT: i32 = 100;
-//const PLAYER_WIDTH: i32 = 100;
-//const PLAYER_HEIGHT: i32 = 100;
+const PROJECTILE_SCALE: f32 = 0.35;
 
-const PROJECTILE_SCALE: f32 = 0.5;
+const PROJECTILE_SPEED: f32 = 400.0;
 
-const PLAYER_SIZE: Vec2 = Vec2::new(0.75*460.0, 0.75*246.0);
+const PLAYER_SIZE: Vec2 = Vec2::new(0.5*460.0, 0.5*246.0);
+
+const PLAYER_SPEED: f32 = 550.0; 
 
 #[derive(Resource)]
 pub struct TextureAssets {
@@ -38,14 +37,14 @@ fn load_textures(asset_server: AssetServer) -> TextureAssets {
     textures.push(asset_server.load("harmful3.png"));
 
     let s = PROJECTILE_SCALE;
-    let mut sizes = vec![Vec2::new(300.0 * s, 185.0 * s),   // food1
-                                    Vec2::new(300.0 * s, 113.0 * s),   // food2
-                                    Vec2::new(300.0 * s, 261.0 * s),   // food3
-                                    Vec2::new(300.0 * s, 153.0 * s),   // food4
-                                    Vec2::new(300.0 * s, 237.0 * s),   // food5
-                                    Vec2::new(300.0 * s, 179.0 * s),   // harmful1
-                                    Vec2::new(300.0 * s, 300.0 * s),   // harmful2
-                                    Vec2::new(255.0 * s, 300.0 * s)];  // harmful3
+    let mut sizes = vec![Vec2::new(300.0 * s, 185.0 * s),   // food1     -  shrimp
+                                    Vec2::new(300.0 * s, 113.0 * s),   // food2     -  fish
+                                    Vec2::new(300.0 * s, 261.0 * s),   // food3     -  algae
+                                    Vec2::new(300.0 * s, 153.0 * s),   // food4     -  worm
+                                    Vec2::new(300.0 * s, 237.0 * s),   // food5     -  sea lettuce?
+                                    Vec2::new(300.0 * s, 179.0 * s),   // harmful1  -  rock
+                                    Vec2::new(300.0 * s, 300.0 * s),   // harmful2  -  fishing hook
+                                    Vec2::new(255.0 * s, 300.0 * s)];  // harmful3  -  tire
 
     TextureAssets { textures, sizes }
 }
@@ -58,13 +57,22 @@ fn seconds_since_epoch() -> u64 {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Seafood Scramble".into(),
+                resolution: (1280., 720.).into(),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        //.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
         .add_systems(Startup, setup)
         .insert_resource(TextureAssets { textures: Vec::new(), sizes: Vec::new() })
         .add_systems(
             Update,
             (
-                spawn_sprite.run_if(on_timer(Duration::from_secs(1))),
+                spawn_projectile.run_if(on_timer(Duration::from_millis(650))),
             )
         )
         .add_systems(Update, update_player)
@@ -92,89 +100,62 @@ fn resize_notificator(resize_event: Res<Events<WindowResized>>) {
         }
     }
 }
-struct ObjectBounds {
-    left: f32,
-    right: f32,
-    top: f32,
-    bottom: f32,
+
+fn check_collision(pos1: Vec2, size1: Vec2, pos2: Vec2, size2: Vec2) -> bool {
+    let left1   = pos1.x - size1.x/2.0;
+    let right1  = pos1.x + size1.x/2.0;
+    let top1    = pos1.y + size1.y/2.0;
+    let bottom1 = pos1.y - size1.y/2.0;
+
+    let left2   = pos2.x - size2.x/2.0;
+    let right2  = pos2.x + size2.x/2.0;
+    let top2    = pos2.y + size2.y/2.0;
+    let bottom2 = pos2.y - size2.y/2.0;
+    
+    // If one rectangle is on the left side of the other
+    if right1 <= left2 || right2 <= left1 {
+        return false;
+    }
+
+    // If one rectangle is above the other
+    if bottom1 >= top2 || bottom2 >= top1 {
+        return false;
+    }
+
+    // If they are neither, then they must be colliding.
+    return true;
 }
 
-fn calculate_bounds(transform: &Transform, sprite: &Sprite, texture: &Texture) -> ObjectBounds {
-    let scale = transform.scale.x; // Assuming uniform scaling
-    let texture_size = Vec2::new(texture.width() as f32, texture.height() as f32);
-    let sprite_size = texture_size * scale;
-    let translation = transform.translation.truncate();
+static mut player_health: i32 = 3;
+static mut player_score: i32 = 0;
 
-    let left = translation.x - sprite_size.x / 2.0;
-    let right = translation.x + sprite_size.x / 2.0;
-    let top = translation.y + sprite_size.y / 2.0;
-    let bottom = translation.y - sprite_size.y / 2.0;
-
-    ObjectBounds {
-        left,
-        right,
-        top,
-        bottom,
-    }
-}
-
-/*fn hit_detection(
-    mut commands: Commands,
-    enemy_query: Query<(Entity, &Transform, Enemy)>,
-    bullet_query: Query<&Transform, With<Bullet>>
-) {
-    for (entity, enemy_transform) in enemy_query.iter() {
-        for bullet_transform in bullet_query.iter() {
-            // Your collision check
-            if ... {
-                commands.entity(entity).despawn();
-            }
-        }
-    }
-}*/
-
-/*
-fn update_projectiles(time: Res<Time>,
-    input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<
-        (
-            &mut Transform,
-            &Sprite,
-            &Projectile,
-        ),
-    >,
-) {
-    for (mut transform, sprite, projectile) in &mut query {
-        if input.pressed(KeyCode::KeyW) {
-            transform.translation.y -= 350.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyS) {
-            transform.translation.y += 350.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyD) {
-            transform.translation.x -= 350.0 * time.delta_seconds();
-        }
-        if input.pressed(KeyCode::KeyA) {
-            transform.translation.x += 350.0 * time.delta_seconds();
-        }
-    }
-}*/
-fn spawn_sprite(mut commands: Commands, texture_assets: Res<TextureAssets>,) {
+fn spawn_projectile(mut commands: Commands, texture_assets: Res<TextureAssets>,) {
     let index = rand::thread_rng().gen_range(0..texture_assets.textures.len());
 
+    let size = texture_assets.sizes[index];
+
+    let screen_height;
+    let screen_width;
+    unsafe {
+        screen_height = HEIGHT;
+        screen_width = WIDTH;
+    }
+    let x = rand::thread_rng().gen_range(-screen_width/2.0 + size.x/2.0..screen_width/2.0 - size.x/2.0);
+    let y = screen_height/2.0 + size.y/2.0;
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
-                custom_size: Some(texture_assets.sizes[index]),
+                custom_size: Some(size),
                 ..default()
             },
+            transform: Transform::from_xyz(x, y, 0.0),
             //transform: Transform::from_scale(Vec3::splat(0.5)),
             texture: texture_assets.textures[index].clone_weak(),
             ..default()
         },
         Projectile {
-            good: true,
-            size: texture_assets.sizes[index],
+            good: index < 5, // if index is less than 5, then its one of the food. otherwise its bad. 
+            size: size,
         },
     ));
 }
@@ -186,6 +167,8 @@ fn update_player(
     input: Res<ButtonInput<KeyCode>>,
     mut query: Query<
         (
+            Entity,
+
             Option<&Player>,
             Option<&Projectile>,
 
@@ -198,28 +181,46 @@ fn update_player(
         ),
     >,
 ) {
-    let mut player_transform;
-    for (player, projectile, indices, mut timer, mut atlas, mut transform, sprite,) in &mut query
+    let mut player_pos = Vec2::ZERO;
+    for (entity, player, projectile, indices, mut timer, mut atlas, mut transform, sprite,) in &mut query
     {
         if let (Some(player), Some(indices), Some(mut timer), Some(mut atlas)) = (player, indices, timer.as_mut(), atlas.as_mut())   // if this is a PLAYER
         {
-            player_transform = transform.clone();
+            let left_bound;
+            let right_bound;
+            unsafe {
+                if player_health <= 0 {
+                    transform.rotation = Quat::from_rotation_z(std::f32::consts::PI)    ; // Rotate 180 degrees;
+                    println!("flipped");
+                    return;
+                }
+                transform.translation.y = -HEIGHT/2.0 + PLAYER_SIZE.y;
+
+                left_bound = -WIDTH/2.0 + PLAYER_SIZE.x/2.0;
+                right_bound = -left_bound;
+            }
+
+            player_pos = Vec2::new(transform.translation.x, transform.translation.y);
 // ======= FERRIS WALKING CODE ========
             let old_x = transform.translation.x;
-            let old_y = transform.translation.y;
-            if input.pressed(KeyCode::KeyW) {
-                transform.translation.y += 350.0 * time.delta_seconds();
-            }
-            if input.pressed(KeyCode::KeyS) {
-                transform.translation.y -= 350.0 * time.delta_seconds();
-            }
+            
+            //if input.pressed(KeyCode::KeyW) {
+            //    transform.translation.y += PLAYER_SPEED * time.delta_seconds();
+            //}
+            //if input.pressed(KeyCode::KeyS) {
+            //    transform.translation.y -= PLAYER_SPEED * time.delta_seconds();
+            //}
             if input.pressed(KeyCode::KeyD) {
-                transform.translation.x += 350.0 * time.delta_seconds();
+                transform.translation.x += PLAYER_SPEED * time.delta_seconds();
             }
             if input.pressed(KeyCode::KeyA) {
-                transform.translation.x -= 350.0 * time.delta_seconds();
+                transform.translation.x -= PLAYER_SPEED * time.delta_seconds();
             }
-            if old_x != transform.translation.x || old_y != transform.translation.y {
+            
+            transform.translation.x = f32::max(left_bound, f32::min(right_bound, transform.translation.x)); // lock it inside the bounds.
+            
+
+            if old_x != transform.translation.x {
                 timer.tick(time.delta());
                 if timer.just_finished() {
                     atlas.index = if atlas.index == indices.last {
@@ -228,25 +229,39 @@ fn update_player(
                         atlas.index + 1
                     };
                 }
-                println!("X = {} Y = {}", transform.translation.x, transform.translation.y);
+                //println!("X = {} Y = {}", transform.translation.x, transform.translation.y);
             } else {
                 atlas.index = indices.first;
             }
         }
     }
 // ======= PROJECTILE MOVEMENT AND COLLISION DETECTION CODE ========
-    for (player, projectile, indices, mut timer, mut atlas, mut transform, sprite,) in &mut query {
+    for (entity, player, projectile, indices, mut timer, mut atlas, mut transform, sprite,) in &mut query {
         if let Some(projectile) = projectile {
-            transform.translation.y -= 70.0 * time.delta_seconds();
+            transform.translation.y -= PROJECTILE_SPEED * time.delta_seconds();
             
+            let screen_height;
             unsafe {
-                if transform.translation.y <= -HEIGHT/2.0 {
-                    transform.translation.y = HEIGHT/2.0;
-                }
+                screen_height = HEIGHT; 
             }
-            
-            // CHECK COLLISION TO PLAYER HERE !
-            
+
+            // CHECK COLLISION TO PLAYER 
+            let projectile_pos = Vec2::new(transform.translation.x, transform.translation.y);
+            if check_collision(player_pos, PLAYER_SIZE, projectile_pos, projectile.size) {
+                println!("collide!");
+                unsafe {
+                    if projectile.good {
+                        player_score+=1;
+                    } else {
+                        player_health-=1;
+                    }
+                }
+                commands.entity(entity).despawn();
+            }
+
+            if transform.translation.y + projectile.size.y/2.0 <= -screen_height/2.0 { // if the top of the projectile is below the bottom of the screen
+                commands.entity(entity).despawn();
+            }
         }
     }
 
@@ -271,12 +286,17 @@ fn setup( mut commands: Commands, asset_server: Res<AssetServer>, mut texture_as
     texture_assets.textures = textures;
     texture_assets.sizes = sizes;
 
+    let background_image = asset_server.load("background.png");
     let texture = asset_server.load("ferris_sprite_sheet.png");
     let layout = TextureAtlasLayout::from_grid(Vec2::new(460.0, 246.0), 3, 1, None, None);
     let texture_atlas_layout = texture_atlas_layouts.add(layout);
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 0, last: 2 };
     commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: background_image,
+        ..default()
+    });
     commands.spawn((
         SpriteBundle {
             //transform: Transform::from_scale(Vec3::splat(0.5)),
@@ -284,6 +304,7 @@ fn setup( mut commands: Commands, asset_server: Res<AssetServer>, mut texture_as
                 custom_size: Some(PLAYER_SIZE),
                 ..default()
             },
+            transform: Transform::from_xyz(0.0, -720.0/2.0 + PLAYER_SIZE.y, 0.0),
             texture,
             ..default()
         },
