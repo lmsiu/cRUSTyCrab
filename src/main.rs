9b19f3db54d53e1a022b49d7d095481e3a938265
Sam M. <56046438+sammiemac@users.noreply.github.com>
2024-05-08 16:33:52 -0700

    fixed game exit issue (kind of)

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() {
         // Declare the game state, whose starting value is determined by the `Default` trait
         .init_state::<GameState>()
         .add_systems(Startup, setup)
+        .add_systems(Update, bevy::window::close_on_esc)
         // Adds the plugins for each state
         .add_plugins((splash::splash_plugin, menu::menu_plugin, game::game_plugin))
         .run();
@@ -102,13 +103,12 @@ mod game {
     use bevy::prelude::*;
     use std::process::Command;
 
-    use super::{despawn_screen, GameState, TEXT_COLOR};
+    use super::{despawn_screen, GameState};
 
     // This plugin will contain the game. In this case, it's just be a screen that will
     // display the current settings for 5 seconds before returning to the menu
     pub fn game_plugin(app: &mut App) {
-        app.add_systems(OnEnter(GameState::Game), game_setup)
-            .add_systems(Update, game.run_if(in_state(GameState::Game)))
+        app.add_systems(OnEnter(GameState::Game), game)
             .add_systems(OnExit(GameState::Game), despawn_screen::<OnGameScreen>);
     }
 
@@ -116,76 +116,7 @@ mod game {
     #[derive(Component)]
     struct OnGameScreen;
 
-    #[derive(Resource, Deref, DerefMut)]
-    struct GameTimer(Timer);
-
-    fn game_setup(
-        mut commands: Commands,
-    ) {
-        commands
-            .spawn((
-                NodeBundle {
-                    style: Style {
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                        // center children
-                        align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    },
-                    ..default()
-                },
-                OnGameScreen,
-            ))
-            .with_children(|parent| {
-                // First create a `NodeBundle` for centering what we want to display
-                parent
-                    .spawn(NodeBundle {
-                        style: Style {
-                            // This will display its children in a column, from top to bottom
-                            flex_direction: FlexDirection::Column,
-                            // `align_items` will align children on the cross axis. Here the main axis is
-                            // vertical (column), so the cross axis is horizontal. This will center the
-                            // children
-                            align_items: AlignItems::Center,
-                            ..default()
-                        },
-                        background_color: Color::BLACK.into(),
-                        ..default()
-                    })
-                    .with_children(|parent| {
-                        // Display two lines of text, the second one with the current settings
-                        parent.spawn(
-                            TextBundle::from_section(
-                                "Will be back to the menu shortly...",
-                                TextStyle {
-                                    font_size: 80.0,
-                                    color: TEXT_COLOR,
-                                    ..default()
-                                },
-                            )
-                            .with_style(Style {
-                                margin: UiRect::all(Val::Px(50.0)),
-                                ..default()
-                            }),
-                        );
-                        
-                    });
-            });
-        // Spawn a 5 seconds timer to trigger going back to the menu
-        commands.insert_resource(GameTimer(Timer::from_seconds(0.5, TimerMode::Once)));
-    }
-
-    // Tick the timer, and change state when finished
-        
-    fn game(
-        time: Res<Time>, 
-        mut game_state: ResMut<NextState<GameState>>, 
-        mut timer: ResMut<GameTimer>
-    ) {
-        if timer.tick(time.delta()).finished() {
-            game_state.set(GameState::Menu);
-        }
+    fn game() {
        Command::new("cargo").arg("run").arg("--bin").arg("gameone").output().expect("unable to run game one");
     }
 }
